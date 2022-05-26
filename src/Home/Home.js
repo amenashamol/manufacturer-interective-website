436253b1928293cb30de8ed5144a71b73c338ba2
amena <amena.ruet@gmail.com>
2022-05-26 18:55:30 +0600

    navber

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+import Banner from './Banner';
+
+const Home = () => {
+    return (
+        <div>
+           <Banner></Banner>
+           <Footer></Footer> 
+
+        </div>
+    );
+};
+
+export default Home;
